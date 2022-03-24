ea20d3a0c20cb241a40433cd485cedc9ca05484f
MasterKelwin <mkelwin@outlook.com>
2022-03-24 15:58:02 -0300

    testes com regex em linguagens de programação

---
+++ teste.js
@@ -0,0 +1,37 @@
+//var target = "11a22b33c";
+//var exp = /(\d\d)(\w)/g;
+var resultado = null;
+
+//while (resultado = exp.exec(target)){
+//    console.log(resultado);
+//    console.log(exp.lastIndex);
+//}
+
+//console.log(resultado)
+
+/////////////////////////////////////////////
+
+//var anoMesDia = '2007-12-31';
+//var exp = /-/g;
+
+//anoMesDia = anoMesDia.replace(exp, '/');
+
+//console.log(anoMesDia)
+
+/////////////////////////////////////////////
+
+//var arquivo = '100,200-150,200;20';
+//var exp = /[,;-]/;
+//var valores = arquivo.split(exp);
+
+//console.log(valores)
+
+/////////////////////////////////////////////
+
+//var codigos = 'A121B12112C12212F12G01';
+//var exp = /[A-Za-z]\d+/g;
+//var codigosExtraidos = codigos.match(exp)
+
+//console.log(codigosExtraidos)
+
+/////////////////////////////////////////////
